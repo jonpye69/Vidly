a54b967450590b73ed9a280c16ba184bb0a320c0
jonpye <jon.pye@nbt.nhs.uk>
2017-07-11 08:32:42 +0100

    Need to refactor start of spa dev (including js and css) but working version, still WIP though

---
+++ VIdly/Scripts/custom/spa.activeScroll.js
@@ -0,0 +1,46 @@
+﻿var isAnimating = false;
+$('.js-spa-menu li a')
+    .on('click',
+        function() {
+            isAnimating = true;
+            $('.js-spa-menu li').removeClass('active');
+            $(this).parent().addClass('active');
+            var scrollAnchor = $(this).attr('data-scroll'),
+                scrollPoint = $('div[data-anchor="' + scrollAnchor + '"]').offset().top - 58; //28
+
+            $('body,html')
+                .animate({
+                        scrollTop: scrollPoint
+                    },
+                    500,
+                    function() {
+                        isAnimating = false;
+                    });
+
+            return false;
+
+        });
+
+
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 50) { //100
+    	$('.js-spa-menu').addClass('fixed');
+    	$('.js-spa-container div').each(function (i) {
+            if ($(this).position().top <= windscroll - 45) {    //60
+
+            if (!isAnimating) {
+            	$('.js-spa-menu li.active').removeClass('active');
+            	$('.js-spa-menu li').eq(i).addClass('active');
+            }
+            }
+        });
+
+    } else {
+
+    	$('.js-spa-menu').removeClass('fixed');
+    	$('.js-spa-menu li.active').removeClass('active');
+    	$('.js-spa-menu li:first').addClass('active');
+    }
+
+}).scroll();
